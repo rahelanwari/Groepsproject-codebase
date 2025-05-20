6551d82f3d1b9607560857204300eddcbf6bb1b8
rahelanwari <560249@student.fontys.nl>
2025-05-20 15:45:24 +0200

    Update __init__.py
    terug gezet

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -2,15 +2,15 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-#class User(db.Model):
-  #  id = db.Column(db.String(6), primary_key=True)
-  #  display_name = db.Column(db.String(50), nullable=False)
-   # public_key = db.Column(db.Text, nullable=False)
-  #  created_at = db.Column(db.DateTime, server_default=db.func.now())
+class User(db.Model):
+    id = db.Column(db.String(6), primary_key=True)
+    display_name = db.Column(db.String(50), nullable=False)
+    public_key = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
     
-    # Add relationship to chats
-   # sent_messages = db.relationship('Chat', backref='sender', foreign_keys='Chat.sender_id')
-   # received_messages = db.relationship('Chat', backref='recipient', foreign_keys='Chat.recipient_id')
+     Add relationship to chats
+    sent_messages = db.relationship('Chat', backref='sender', foreign_keys='Chat.sender_id')
+    received_messages = db.relationship('Chat', backref='recipient', foreign_keys='Chat.recipient_id')
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
