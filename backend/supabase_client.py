e4763226215cad38b26d1b719958f9ba568ba881
rahelanwari <560249@student.fontys.nl>
2025-05-21 20:48:46 +0200

    Create supabase_client.py

---
+++ backend/supabase_client.py
@@ -0,0 +1,6 @@
+import os
+from supabase import create_client, Client
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.environ.get("SUPABASE_SERVICE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
